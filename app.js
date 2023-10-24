1492f950f5ef61d28bedcd3b9ca6a91a746de673
Adolfo-Contreras <adolfo.alvaradocontreras1760@stu.mtec.edu>
2023-10-24 07:43:32 -0600

    got the functions to use written down

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 "use strict";
+//get course data
+
+//calculate course data
+function calcCourseData(){};
+//populate tables with info
+function addTblInfo(){};
+//populate table with players
+function addPlayers(){};
+//calculate player data
+function calcPlayerData(){};
+//create the tables
+function renderTbl(){}
+//ANIMATIONS AND STYLE STUFF GO HERE
+
+    //change color palates
+
+    //animate top menu
