c40ff08f524b7c7ecab2ce938a0f4c677788ea0d
Adolfo-Contreras <adolfo.alvaradocontreras1760@stu.mtec.edu>
2023-10-26 08:54:24 -0600

    api function made but not refined

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 "use strict";
 //get course data
-
-//calculate course data
-function calcCourseData(){};
+function getCourse(courseid){
+    return fetch(
+        `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${courseid}.json`,
+        ).then(function (response) {
+            if(!response.ok) {
+                throw new Error('Network response was not ok');
+              }else{return response.json()}
+        }
+    ).then(function (data){
+        return data
+    }).catch(function (error){
+        console.error('Error:',error);
+    })
+}
+let FoxGolf = document.getElementById('FoxGolf');
+FoxGolf.addEventListener('click',()=>{
+    console.log(FoxGolf.value);
+    console.log(getCourse(FoxGolf.value));
+});
 //populate tables with info
 function addTblInfo(){};
+//calculate course data
+function calcCourseData(){};
 //populate table with players
 function addPlayers(){};
 
